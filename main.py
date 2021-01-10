7e254cac35eee75925c28662c480fb9b9b3b0830
PYJS123 <73827861+PYJS123@users.noreply.github.com>
2021-01-10 13:53:05 +0000

    Putting up the canvas
    I have:
    -Settled on an appropriate window size based on different computer sizes
    -Gotten the `draw` and `update` functions in

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 This is a replica of the Oldie's game, pong. In Pygame Zero, a Python Library,
 mainly used for making games and software with graphical aspects.
 """
+
+WIDTH = 1100
+HEIGHT = 650
+
+def draw():
+    pass
+
+def update():
+    pass
